b9799c990f0f18a249cf9fabf35daab56bdd6443
Kruithne <personal@stanleybatch.co.uk>
2012-04-07 03:12:25 +0100

    Moved to Runsafe Framework, added lots of things and cleaned code.

---
+++ src/me/Kruithne/MessageCycle/Constants.java
@@ -0,0 +1,13 @@
+package me.Kruithne.MessageCycle;
+
+import org.bukkit.ChatColor;
+
+public class Constants
+{
+
+	public static String broadcastFormat = ChatColor.GOLD + "[Server]: " + ChatColor.YELLOW + "%s";
+	
+	public static String defaultConfigurationFile = "defaultConfig.yml";
+	public static String configurationFile = "plugins/MessageCycle/config.yml";
+	
+}
---
+++ src/me/Kruithne/MessageCycle/MessageCycler.java
@@ -0,0 +1,94 @@
+package me.Kruithne.MessageCycle;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import no.runsafe.framework.interfaces.IConfiguration;
+import no.runsafe.framework.interfaces.IOutput;
+import no.runsafe.framework.interfaces.IPluginDisabled;
+import no.runsafe.framework.interfaces.IPluginEnabled;
+import no.runsafe.framework.interfaces.IScheduler;
+
+public class MessageCycler implements IPluginEnabled, IPluginDisabled
+{
+	private boolean cycleEnabled = false;
+	private IConfiguration config;
+	private IScheduler scheduler;
+	private IOutput output;
+	private ArrayList<String> messages;
+	private Iterator<String> messageIterator;
+	private int messageDelay;
+	
+	public MessageCycler(IConfiguration config, IScheduler scheduler, IOutput output)
+	{
+		this.config = config;
+		this.scheduler = scheduler;
+		this.output = output;
+	}
+
+	@Override
+	public void OnPluginDisabled()
+	{
+		this.stopCycle();
+	}
+
+	@Override
+	public void OnPluginEnabled()
+	{
+		this.loadDataFromConfig();
+		this.startCycle();
+	}
+	
+	private void startCycle()
+	{
+		this.cycleEnabled = true;
+		this.setupIterator();
+	}
+	
+	private void stopCycle()
+	{
+		this.cycleEnabled = false;
+		this.messageIterator = null;
+		this.messages.clear();
+	}
+	
+	private void setupIterator()
+	{
+		this.messageIterator = this.messages.iterator();
+	}
+	
+	private void loadDataFromConfig()
+	{
+		this.messages = (ArrayList<String>) this.config.getConfigValueAsList("messages");
+		this.messageDelay = this.config.getConfigValueAsInt("cycleTime");
+	}
+	
+	private void broadcastNextMessage()
+	{
+		if (!this.messageIterator.hasNext())
+		{
+			this.setupIterator();
+		}
+		
+		if (this.messageIterator.hasNext())
+		{
+			this.output.outputToServer(String.format(Constants.broadcastFormat, this.messageIterator.next()));
+		}
+		
+		this.registerNewMessage();
+	}
+	
+	private void registerNewMessage()
+	{
+		if (this.cycleEnabled)
+		{
+			this.scheduler.setTimedEvent(new Runnable() {
+				public void run()
+				{
+					broadcastNextMessage();
+				}
+			}, this.messageDelay);
+		}
+	}
+
+}
