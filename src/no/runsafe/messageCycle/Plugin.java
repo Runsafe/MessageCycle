573dcbfef8546038ab53372e705bdd20f748046d
Morten Nilsen <morten@runsafe.no>
2013-12-15 12:44:14 +0100

    Declare features used

--- src/no/runsafe/messageCycle/Plugin.java
+++ src/no/runsafe/messageCycle/Plugin.java
@@ -1,12 +1,14 @@
 package no.runsafe.messageCycle;
 
 import no.runsafe.framework.RunsafeConfigurablePlugin;
+import no.runsafe.framework.features.Events;
 
 public class Plugin extends RunsafeConfigurablePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
+		addComponent(Events.class);
 		this.addComponent(MessageCycler.class);
 	}
 }
